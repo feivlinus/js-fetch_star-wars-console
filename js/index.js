1840b8c75bf7dd3159c5de636027c6ad208206ab
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-10 16:25:32 +0200

    challenge completed

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData(url) {
+  const response = await fetch(url);
+  await console.log("Status: ", response.status, "Ok? ", response.ok);
+  await console.log("RAW Response: ", response);
+  const data = await response.json();
+  return data.results;
+}
 
-fetchData();
+const fetchedAndParsedData = await fetchData(url);
+await console.log("Response.results: ", fetchedAndParsedData);
+
+await console.log(
+  "R2-D2 eye color: ",
+  fetchedAndParsedData.find((element) => element.name === "R2-D2").eye_color
+);
